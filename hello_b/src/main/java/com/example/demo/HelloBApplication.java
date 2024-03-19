bb25bd143e2881ae4cef75589d1c35ad75f056b8
Lee-Yongje <dldydwp9024@gmail.com>
2024-03-19 15:35:40 +0900

    BoardController 생성

---
+++ hello_b/src/main/java/com/example/demo/HelloBApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBApplication.class, args);
+	}
+
+}
